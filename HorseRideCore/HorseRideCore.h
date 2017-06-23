a42f609214a65e1d94993ca83311fc42dd3eedff
Vince Yuan <vince.yuan@gmail.com>
2017-06-23 15:37:30 +0800

    Add SiriExtension, SiriExtensionUI, HorseRideCore.

---
+++ HorseRideCore/HorseRideCore.h
@@ -0,0 +1,19 @@
+//
+//  HorseRideCore.h
+//  HorseRideCore
+//
+//  Created by Vince Yuan on 6/23/17.
+//  Copyright © 2017 Vince Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HorseRideCore.
+FOUNDATION_EXPORT double HorseRideCoreVersionNumber;
+
+//! Project version string for HorseRideCore.
+FOUNDATION_EXPORT const unsigned char HorseRideCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HorseRideCore/PublicHeader.h>
+
+
